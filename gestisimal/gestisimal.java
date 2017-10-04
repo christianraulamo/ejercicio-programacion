94698af996dc4da2d3507c783f4c84ffad19f11c
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-04 10:54:36 +0200

    Rama con proyecto.md.

---
+++ gestisimal/gestisimal.java
@@ -0,0 +1,13 @@
+/**
+ * Muestra por pantalla la frase "¡Hola mundo!"
+ *
+ * @author Christian Amo
+ */
+
+public class gestisimal { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("G E S T I S I M A L");
+    System.out.println("1 Cajas");
+    System.out.println("1 Estantes");
+  }
+}
