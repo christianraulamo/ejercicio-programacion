3431e16e25eef36b265db46986bc1947795f8456
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-09 17:25:42 +0200

    Añadido ejercicio 05

---
+++ Tema 3/Ejercicio05.java
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio 05
+ *
+ * @author Christian Amo
+ */
+ 
+public class Ejercicio05 {
+  public static void main(String[] args) {
+
+    System.out.println("Área de un rectángulo");
+    System.out.print("Por favor, introduzca la longitud de la base (cm): ");
+    double base = Double.parseDouble(System.console().readLine());
+    System.out.print("Introduzca la altura (cm): ");
+    double altura = Double.parseDouble(System.console().readLine());
+    System.out.println("El área del rectángulo es " + (base * altura) + " cm2");
+  }
+}
