d4a0dcc2ac4515e6e5c7f05239fc695ccf1a75af
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-09 09:45:11 +0200

    Añadido Ejercicio01 tema 3

---
+++ Tema 3/Ejercicio01.java
@@ -0,0 +1,22 @@
+/**
+ * Muestra por pantalla la frase "¡Hola mundo!"
+ *
+ * @author Christian Amo
+ */
+ 
+public class Ejercicio01 {
+  public static void main(String[] args) {
+    int x;
+    int y;
+    String linea;
+
+    System.out.print("Por favor, introduce el primer número: ");
+    linea = System.console().readLine();
+    x = Integer.parseInt( linea );
+    System.out.print("Introduce el segundo número: ");
+    linea = System.console().readLine();
+    y = Integer.parseInt( linea );
+    
+    System.out.println(x + "*" + y + "=" + (x * y));
+  }
+}
