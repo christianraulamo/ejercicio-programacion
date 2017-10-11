d70dd574e8a8b877083d1c9a65a53c94bb836520
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-11 10:11:28 +0200

    Añadido carpeta tema 4 y el ejercicio 01

---
+++ Tema 4/Ejercicio01.java
@@ -0,0 +1,41 @@
+/**
+ * Ejercicio 01
+ *
+ * @author Christian Amo
+ */
+ 
+public class Ejercicio01 {
+  public static void main(String[] args) {
+	  
+	  String dia;
+
+    System.out.print("Por favor, introduzca un día de la semana y le diré qué asignatura toca a primera hora ese día: ");
+    dia = (System.console().readLine()); 
+    dia.toLowerCase(); // convierto a minúsculas todas las letras
+    
+    switch(dia) {
+      case "lunes":
+        System.out.println("Programación y Bases de Datos");
+        break;
+      case "martes":
+        System.out.println("Sistema Informatico, Lenguaje de Marcas y Programación");
+        break;
+      case "miércoles":
+        System.out.println("Programación y Base de Datos");
+        break;
+      case "jueves":
+        System.out.println("Entorno de Desarrollo y Lenguaje de Marcas");
+        break;
+      case "viernes":
+        System.out.println("Sistema Informatico y Formación profesional");
+        break;
+      case "sábado":
+        // continúa debajo
+      case "domingo":
+        System.out.println("¡Ese día no hay clase!");
+        break;
+      default:
+        System.out.println("El día introducido no es correcto.");
+    }
+  }
+}
