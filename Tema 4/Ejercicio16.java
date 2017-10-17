f9754855f26b1d26a5f22afec95dce63ae2106ab
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-17 13:24:34 +0200

    Corrección del ejercicio 16

--- Tema 4/Ejercicio16.java
+++ Tema 4/Ejercicio16.java
@@ -17,7 +17,7 @@ public class Ejercicio16 {
     System.out.println(" de que tu pareja está siendo infiel.\n");
 
     System.out.print("1. Tu pareja parece estar más inquieta de lo");
-    System.out.print(" normal sin ningún motivo aparente.\n(v) se es");
+    System.out.print(" normal sin ningún motivo aparente.\n(v) si es");
     System.out.print(" verdadero o (f) se es falso: ");
     respuesta = System.console().readLine();
     if ( respuesta.equals("v") ) {
@@ -25,14 +25,15 @@ public class Ejercicio16 {
     }
 
     System.out.print("2. Ha aumentado sus gastos de vestuario");
-    System.out.print(".\n(v)erdadero o (f)also: ");
+    System.out.print(".\n(v) si es verdadero o (f) si es falso: ");
     respuesta = System.console().readLine();
     if ( respuesta.equals("v") ) {
       puntos += 3;
     }
     
     System.out.print("3. Ha perdido el interés que mostraba ");
-    System.out.print("anteriormente por ti.\n(v)erdadero o (f)also: ");
+    System.out.print("anteriormente por ti.\n(v) si es verdadero o");
+    System.out.print(" (f) si es falso: ");
     respuesta = System.console().readLine();
     if ( respuesta.equals("v") ) {
       puntos += 3;
@@ -41,7 +42,7 @@ public class Ejercicio16 {
     System.out.print("4. Ahora se afeita y se asea con más frecuencia");
     System.out.print(" (si es hombre) o ahora se arregla el pelo y");
     System.out.print(" se asea con más frecuencia (si es mujer)");
-    System.out.print(".\n(v)erdadero o (f)also: ");
+    System.out.print(".\n(v) si es verdadero o (f) si es falso: ");
     respuesta = System.console().readLine();
     if ( respuesta.equals("v") ) {
       puntos += 3;
@@ -55,39 +56,39 @@ public class Ejercicio16 {
     }
     
     System.out.print("6. A veces tiene llamadas que dice no querer");
-    System.out.print(" contestar cuando estás tú delante.\n(v)erdadero");
-    System.out.print(" o (f)also: ");
+    System.out.print(" contestar cuando estás tú delante.\n(v) si es");
+    System.out.print(" verdadero o (f) si es falso: ");
     respuesta = System.console().readLine();
     if ( respuesta.equals("v") ) {
       puntos += 3;
     }
         
     System.out.print("7. Últimamente se preocupa más en cuidar la");
-    System.out.println(" línea y/o estar bronceado/a.\n(v)erdadero");
-    System.out.println(" o (f)also: ");
+    System.out.println(" línea y/o estar bronceado/a.\n(v) si es");
+    System.out.print(" verdadero o (f) si es falso: ");
     respuesta = System.console().readLine();
     if ( respuesta.equals("v") ) {
       puntos += 3;
     }
     
     System.out.print("8. Muchos días viene tarde después de trabajar");
-    System.out.print(" porque dice tener mucho más trabajo.\n(v)");
-    System.out.print("erdadero o (f)also: ");
+    System.out.print(" porque dice tener mucho más trabajo.\n(v) si");
+    System.out.print(" es verdadero o (f) si es falso: ");
     respuesta = System.console().readLine();
     if ( respuesta.equals("v") ) {
       puntos += 3;
     }
     
     System.out.print("9. Has notado que últimamente se perfuma");
-    System.out.print(" más.\n(v)erdadero o (f)also: ");
+    System.out.print(" más.\n(v) si es verdadero o (f) si es falso: ");
     respuesta = System.console().readLine();
     if ( respuesta.equals("v") ) {
       puntos += 3;
     }
     
     System.out.print("10. Se confunde y te dice que ha estado en");
-    System.out.print(" sitios donde no ha ido contigo.\n(v)erdadero");
-    System.out.print(" o (f)also: ");
+    System.out.print(" sitios donde no ha ido contigo.\n(v) si es");
+    System.out.print(" verdadero o (f) si es falso: ");
     respuesta = System.console().readLine();
     if ( respuesta.equals("v") ) {
       puntos += 3;
