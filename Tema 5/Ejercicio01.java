d1f7a11904106d9a829a31440a8a905a1f5716fa
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-18 09:00:02 +0200

    Añadido ejercicio 01 y el 04 del tema 5

--- Tema 5/Ejercicio01.java
+++ Tema 5/Ejercicio01.java
@@ -7,6 +7,8 @@
 public class Ejercicio01 {
 	
 	public static void main (String args[]) {
+		
+		System.out.println("Multiplo de 5 hasta el 100");
 		for (int i = 0; i <= 100; i = i + 5) {
 			
 			System.out.println(i);
--- Tema 5/Ejercicio04.java
+++ Tema 5/Ejercicio04.java
@@ -8,6 +8,8 @@ public class Ejercicio04 {
 	
 	public static void main (String args[]) {
 		
+		System.out.print("Numeros del 320 al 160 de 20 en 20");
+		
 		for (int i = 320; i >= 160; i = i - 20) {
 			
 			System.out.println(i);
