c4a9d3c46b34468e67dae5c6746449725d60c6c0
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-18 09:31:31 +0200

    Añadido ejercicio 11 del tema 5

---
+++ Tema 5/Ejercicio11.java
@@ -0,0 +1,19 @@
+/**
+ * Ejercicio 011
+ *
+ * @author Christian Amo
+ */
+
+public class Ejercicio11 {
+	
+	public static void main (String args[]) {
+	  System.out.print("Meta un numero entero: ");
+	  int numeroIntroducido = Integer.parseInt(System.console().readLine());
+	  
+	  for (int i = numeroIntroducido; i < numeroIntroducido + 5; i++) {
+		  
+	  System.out.printf("%4d %6d %8d\n", i, i * i, i * i * i);
+	   }
+	}
+}
+
