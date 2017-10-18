f2aec873bcf4e5bd782b3b7a68247ba10cff0b1e
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-18 08:50:04 +0200

    Añadido ejercicio 01 del tema 5

---
+++ Tema 5/Ejercicio01.java
@@ -0,0 +1,16 @@
+/**
+ * Ejercicio 01
+ *
+ * @author Christian Amo
+ */
+
+public class Ejercicio01 {
+	
+	public static void main (String args[]) {
+		for (int i = 0; i <= 100; i = i + 5) {
+			
+			System.out.println(i);
+		}
+	}
+}
+
