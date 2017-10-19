cfc83ae6f698b863ba6f87499196f57164de8c93
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-19 09:38:52 +0200

    Añadido ejercicio 03 del tema 5

---
+++ Tema 5/Ejercicio03.java
@@ -0,0 +1,20 @@
+/**
+ * Ejercicio 03
+ *
+ * @author Christian Amo
+ */
+
+public class Ejercicio03 {
+	
+	public static void main (String args[]) {
+		       
+    int i = 0;
+    
+    do {
+      System.out.println(i);
+      i = i + 5;
+    } while (i <= 100);
+		
+	}
+}
+
