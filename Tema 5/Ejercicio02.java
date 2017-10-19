0235d7d2227035167d97a92c50e717f215d60e7e
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-19 09:36:32 +0200

    Añadido ejercicio 02 del tema 5

---
+++ Tema 5/Ejercicio02.java
@@ -0,0 +1,22 @@
+/**
+ * Ejercicio 02
+ *
+ * @author Christian Amo
+ */
+
+
+
+public class Ejercicio02 {
+	
+	public static void main (String args[]) {
+		        
+    int i = 0;
+    
+    while (i <= 100) {
+      System.out.println(i);
+      i = i + 5;
+    }
+		
+	}
+}
+
