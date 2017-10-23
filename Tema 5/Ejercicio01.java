70fa15095f95427a3b3c6f2423db8a0e1f11a29f
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-23 08:43:29 +0200

    Corrección del ejercicio 01 del tema 5

--- Tema 5/Ejercicio01.java
+++ Tema 5/Ejercicio01.java
@@ -9,10 +9,11 @@ public class Ejercicio01 {
 	public static void main (String args[]) {
 		
 		System.out.println("Multiplo de 5 hasta el 100");
-		for (int i = 0; i <= 100; i = i + 5) {
+		for (int i = 0; i <= 100; i = i + 5) 
+		
 			
-			System.out.println(i);
+			System.out.print(i + " - ");
 		}
 	}
-}
+
 
