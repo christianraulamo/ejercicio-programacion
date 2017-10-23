85d2e5746c778436534bb436e33da7b865ab1f71
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-23 09:34:05 +0200

    Corrección del ejercicio 15 del tema 5

--- Tema 5/Ejercicio15.java
+++ Tema 5/Ejercicio15.java
@@ -6,18 +6,18 @@
 
 
 public class Ejercicio15 {
-	
-	public static void main (String args[]) {
-		System.out.print("Meta un número real como base: ");
-		double base = Double.parseDouble(System.console().readLine());
-		
-		System.out.print("Meta un número entero positivo como exponencial: ");
-		int exponenteFinal = Integer.parseInt(System.console().readLine());
-		
-	 double potencia;
-	 int exponente;
-	 
-	  for (int i = 1; i <= exponenteFinal; i++) {
+  
+  public static void main (String args[]) {
+    System.out.print("Meta un número real como base: ");
+    double base = Double.parseDouble(System.console().readLine());
+    
+    System.out.print("Meta un número entero positivo como exponencial: ");
+    int exponenteFinal = Integer.parseInt(System.console().readLine());
+    
+  double potencia;
+  int exponente;
+  
+    for (int i = 1; i <= exponenteFinal; i++) {
         
       potencia = 1;
       exponente = i;
@@ -28,7 +28,7 @@ public class Ejercicio15 {
       
       System.out.println(base + "^" + i + " = " + potencia);
      }
-	 
-	}
+  
+  }
 }
 
