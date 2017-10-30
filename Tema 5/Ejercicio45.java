3c727f4fb27661b113dc8d5595aefc0795c95ae7
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2017-10-30 10:57:12 +0100

    Actualización de ejercicios

---
+++ Tema 5/Ejercicio45.java
@@ -0,0 +1,37 @@
+/**
+ * Ejercicio 45
+ *
+ * @author Christian Amo
+ */
+ 
+public class Ejercicio45 {
+  
+  public static void main (String args[]) {
+    
+    System.out.print("Meta un número entero positivo: ");
+    long numeroIntroducido = Long.parseLong(System.console().readLine());
+
+    System.out.print("Meta la posición donde quiere insertar: ");
+    long posicion = Long.parseLong(System.console().readLine());
+
+    System.out.print("Meta el dígito que quiere insertar: ");
+    long digito = Long.parseLong(System.console().readLine());
+
+    long numero = numeroIntroducido;
+
+    int longitud = 0;
+    
+    do {
+      numero /= 10;
+      longitud++;
+    } while (numero > 0);
+    
+    long parteIzquierda = numeroIntroducido / (long)(Math.pow(10, longitud - posicion + 1));
+    parteIzquierda = parteIzquierda * 10 + digito;
+
+    long parteDerecha = numeroIntroducido % (long)(Math.pow(10, longitud - posicion));
+
+    numero = parteIzquierda * (long)(Math.pow(10, longitud - posicion)) + parteDerecha;
+    System.out.print("El número resultante es " + numero);
+  }
+}
