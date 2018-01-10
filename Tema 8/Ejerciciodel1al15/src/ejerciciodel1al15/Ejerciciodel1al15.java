671a04e0b4b076f116b59e5a2fd65e45584b38be
christianraulamo <christianraulamo24@gmail.com>
2018-01-10 10:46:01 +0100

    Actualización de ejercicios

--- Tema 8/Ejerciciodel1al15/src/ejerciciodel1al15/Ejerciciodel1al15.java
+++ Tema 8/Ejerciciodel1al15/src/ejerciciodel1al15/Ejerciciodel1al15.java
@@ -1,15 +1,64 @@
 package ejerciciodel1al15;
 
 /**
- *Ejercicio 
+ * Ejercicio
  *
  * @author christian
  */
-
+import ejerciciodel1al15.matematicas.varia;
 
 public class Ejerciciodel1al15 {
-  
+
     public static void main(String[] args) {
+
+        // Capicua ////////////////////////////////////////
+        if (ejerciciodel1al15.matematicas.varia.esCapicua(451)) {
+            System.out.println("El 451 es capicúa");
+        }
+
+        if (ejerciciodel1al15.matematicas.varia.esCapicua(237732)) {
+            System.out.println("El 237732 es capicúa");
+        }
+        if (ejerciciodel1al15.matematicas.varia.esCapicua(2732)) {
+            System.out.println("El 2732 es capicúa");
+        }
+        if (ejerciciodel1al15.matematicas.varia.esCapicua(110011)) {
+            System.out.println("El 110011 es capicúa");
+        }
+
+        // esPrimo///////////////////////////////////////////
+        if (ejerciciodel1al15.matematicas.varia.esPrimo(11)) {
+            System.out.println("El 11 es primo");
+        }
+
+        if (ejerciciodel1al15.matematicas.varia.esPrimo(834)) {
+            System.out.println("El 834 es primo");
+        }
+        if (ejerciciodel1al15.matematicas.varia.esPrimo(31)) {
+            System.out.println("El 31 es primo");
+        }
+        if (ejerciciodel1al15.matematicas.varia.esPrimo(25)) {
+            System.out.println("El 25 es primo");
+        }
+
+        // elSiguienteEsPrimo /////////////////////////////////
+        System.out.println("El siguiente primo mayor a 31 es " + ejerciciodel1al15.matematicas.varia.siguientePrimo(31));
+        System.out.println("El siguiente primo mayor a 89 es " + ejerciciodel1al15.matematicas.varia.siguientePrimo(89));
+        System.out.println("El siguiente primo mayor a 55 es " + ejerciciodel1al15.matematicas.varia.siguientePrimo(55));
+        System.out.println("El siguiente primo mayor a 68 es " + ejerciciodel1al15.matematicas.varia.siguientePrimo(68));
+
+        // Potencia //////////////////////////////////////////
+        System.out.println("2^10 = " + ejerciciodel1al15.matematicas.varia.potencia(5, 11));
+        System.out.println("5^(-3) = " + ejerciciodel1al15.matematicas.varia.potencia(7, 3));
+        System.out.println("10^6 = " + ejerciciodel1al15.matematicas.varia.potencia(9, -6));
+
+        // digitos ////////////////////////////////////////////////
+        System.out.println("El número 65 tiene " + ejerciciodel1al15.matematicas.varia.digitos(65) + " dígito/s.");
+        System.out.println("El número 456787654 tiene " + ejerciciodel1al15.matematicas.varia.digitos(456787654) + " dígito/s.");
+        System.out.println("El número 1 tiene " + ejerciciodel1al15.matematicas.varia.digitos(1) + " dígito/s.");
+        System.out.println("El número 7248842 tiene " + ejerciciodel1al15.matematicas.varia.digitos(7248842) + " dígito/s.");
+        
+        // voltea /////////////////////////////////////////////////
 
     }
 
