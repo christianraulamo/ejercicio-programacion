148ab7db4bfcb086f818dd2af520d6788b81ce07
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2018-01-22 10:46:00 +0100

    Actualización de ejercicios

--- Tema 8/ejerciciosdel29al34/ejerciciosdel29al34.java
+++ Tema 8/ejerciciosdel29al34/ejerciciosdel29al34.java
@@ -6,22 +6,23 @@ package ejerciciosdel29al34;
  * @author christian
  */
 import ejerciciosdel29al34.matematicas.Varias;
+import java.util.Scanner;
 
 public class ejerciciosdel29al34 {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
         int[][] a = Varias.generaArrayBiInt(5, 8, 0, 100);
 
         Varias.muestraArrayBiInt(a);
         System.out.print("\nFila 2: ");
-
         ejerciciodel20al28.matematicas.Varias.muestraArrayInt(Varias.filaDeArrayBiInt(a, 2));
+        
         System.out.print("\nColumna 6: ");
-
         ejerciciodel20al28.matematicas.Varias.muestraArrayInt(Varias.columnaDeArrayBiInt(a, 6));
+        
         System.out.print("\nCoordenadas del 24 (fila, columna): ");
-
         ejerciciodel20al28.matematicas.Varias.muestraArrayInt(Varias.coordenadasEnArrayBiInt(a, 24));
 
         int[][] b = {{11, 10, 9}, {4, 5, 7}, {2, 6, 1}};
@@ -36,19 +37,20 @@ public class ejerciciosdel29al34 {
                 }
             }
         }
+        
+        
 
         Varias.muestraArrayBiInt(a);
         System.out.print("\nFila: ");
-
-        int fila = Integer.parseInt(System.console().readLine());
+        int fila = s.nextInt();
+        
         System.out.print("Columna: ");
-
-        int columna = Integer.parseInt(System.console().readLine());
+        int columna = s.nextInt();
+        
         System.out.print("Dirección (nose/neso): ");
-
-        String direccion = System.console().readLine();
+        String direccion = s.next();
+        
         System.out.print("\nDiagonal: ");
-
         ejerciciodel20al28.matematicas.Varias.muestraArrayInt(Varias.diagonal(a, fila, columna, direccion));
     }
 
