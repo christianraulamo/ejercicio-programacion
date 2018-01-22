cb38fd9bc992014db32c572544db84ea4f423368
christianraulamo <christianraulamo24@gmail.com>
2018-01-22 09:27:59 +0100

    Actualización de ejercicios

--- Tema 8/ejerciciosdel29al34/ejerciciosdel29al34.java
+++ Tema 8/ejerciciosdel29al34/ejerciciosdel29al34.java
@@ -1,16 +1,55 @@
 package ejerciciosdel29al34;
 
 /**
- *Ejercicio 
+ * Ejercicio
  *
  * @author christian
  */
-
 import ejerciciosdel29al34.matematicas.Varias;
+
 public class ejerciciosdel29al34 {
-  
+
     public static void main(String[] args) {
 
+        int[][] a = Varias.generaArrayBiInt(5, 8, 0, 100);
+
+        Varias.muestraArrayBiInt(a);
+        System.out.print("\nFila 2: ");
+
+        ejerciciodel20al28.matematicas.Varias.muestraArrayInt(Varias.filaDeArrayBiInt(a, 2));
+        System.out.print("\nColumna 6: ");
+
+        ejerciciodel20al28.matematicas.Varias.muestraArrayInt(Varias.columnaDeArrayBiInt(a, 6));
+        System.out.print("\nCoordenadas del 24 (fila, columna): ");
+
+        ejerciciodel20al28.matematicas.Varias.muestraArrayInt(Varias.coordenadasEnArrayBiInt(a, 24));
+
+        int[][] b = {{11, 10, 9}, {4, 5, 7}, {2, 6, 1}};
+
+        Varias.muestraArrayBiInt(b);
+        System.out.println("\nBusca los puntos de silla: ");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (Varias.esPuntoDeSilla(b, i, j)) {
+                    System.out.println("fila " + i + ", columna " + j + " -> " + b[i][j]);
+                }
+            }
+        }
+
+        Varias.muestraArrayBiInt(a);
+        System.out.print("\nFila: ");
+
+        int fila = Integer.parseInt(System.console().readLine());
+        System.out.print("Columna: ");
+
+        int columna = Integer.parseInt(System.console().readLine());
+        System.out.print("Dirección (nose/neso): ");
+
+        String direccion = System.console().readLine();
+        System.out.print("\nDiagonal: ");
+
+        ejerciciodel20al28.matematicas.Varias.muestraArrayInt(Varias.diagonal(a, fila, columna, direccion));
     }
 
 }
