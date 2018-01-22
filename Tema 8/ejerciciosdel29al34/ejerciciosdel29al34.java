5f46327d2ca9c847b2e5742fe2a8ebae30fb74ba
christianraulamo <christianraulamo24@gmail.com>
2018-01-22 08:58:30 +0100

    Actualización de ejercicios

---
+++ Tema 8/ejerciciosdel29al34/ejerciciosdel29al34.java
@@ -0,0 +1,16 @@
+package ejerciciosdel29al34;
+
+/**
+ *Ejercicio 
+ *
+ * @author christian
+ */
+
+import ejerciciosdel29al34.matematicas.Varias;
+public class ejerciciosdel29al34 {
+  
+    public static void main(String[] args) {
+
+    }
+
+}
